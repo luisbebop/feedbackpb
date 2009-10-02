3a0c7365240ffe58064075b498fbe222d0af0263
luisbebop <luisbebop@gmail.com>
2009-10-02 11:13:26 -0300

    Error on changing table on heroku. Trying to fix the problem.

--- db/migrate/20090929230608_create_feedbacks.rb
+++ db/migrate/20090929230608_create_feedbacks.rb
@@ -1,9 +1,9 @@
 class CreateFeedbacks < ActiveRecord::Migration
   def self.up
     create_table :feedbacks do |t|
-      t.string :start
-      t.string :continue
-      t.string :stop
+      t.text :start, :limit => 2048
+      t.text :continue, :limit => 2048
+      t.text :stop, :limit => 2048
       t.references :user
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091002133918) do
+ActiveRecord::Schema.define(:version => 20090929230608) do
 
   create_table "feedbacks", :force => true do |t|
     t.text     "start",      :limit => 2048
